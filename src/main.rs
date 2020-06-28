49a8042a36fdcf0bc6d4f2d6ee0c0b05d14c213a
Will Thomas <technimanx@gmail.com>
2020-06-28 12:50:50 +0100

    Added dumb monsters

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rltk::{ GameState, Rltk };
+use rltk::{ GameState, Rltk, RGB };
 use specs::prelude::*;
 
 mod colours;
@@ -34,8 +34,12 @@ impl GameState for State {
         // render entities
         let positions = self.world.read_storage::<Position>();
         let render_datas = self.world.read_storage::<RenderData>();
+        let map = self.world.fetch::<Map>();
         for (pos, render) in (&positions, &render_datas).join() {
-            context.set(pos.x, pos.y, render.foreground, render.background, render.glyph);
+            let idx = map.xy_idx(pos.x, pos.y);
+            if map.visible_tiles[idx] {
+                context.set(pos.x, pos.y, render.foreground, render.background, render.glyph);
+            }
         }
     }
 }
@@ -63,12 +67,34 @@ fn main() -> rltk::BError {
     // register components
     register_components(&mut game_state.world);
 
-    // insert resources
+    // create map
     let map: Map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].centre();
-    game_state.world.insert(map);
-
+    
     // add entities
+    let mut rng = rltk::RandomNumberGenerator::new();
+    for room in map.rooms.iter().skip(1) {
+        let (x, y) = room.centre();
+        let glyph: rltk::FontCharType;
+        let col: RGB;
+        let roll = rng.roll_dice(1, 2);
+        match roll {
+            1 => { glyph = rltk::to_cp437('g'); col = COLOURS[11]; }
+            _ => { glyph = rltk::to_cp437('o'); col = COLOURS[3]; }
+        }
+
+        game_state.world.create_entity()
+            .with(Position{ x, y })
+            .with(RenderData {
+                glyph: glyph,
+                foreground: col,
+                background: COLOURS[0]
+            })
+            .with(Viewshed::new(8))
+            .build();
+    }
+
+    // add player
     game_state.world.create_entity()
         .with(Position { x: player_x, y: player_y })
         .with(RenderData {
@@ -79,6 +105,9 @@ fn main() -> rltk::BError {
         .with(Player {})
         .with(Viewshed::new(8))
         .build();
+
+    // insert resources
+    game_state.world.insert(map);
 
     // fire off main loop
     rltk::main_loop(context, game_state)
